70efc239200cfa4ff7d3a08875943463c32a3fda
Taman Gill <tgill0424@gmail.com>
2023-07-24 10:51:25 -0700

    fixed database

--- Models/User.cs
+++ Models/User.cs
@@ -4,7 +4,7 @@ public class User
     public int Id { get; set; }
 
     // Properties
-    public string? Name { get; set; }
-    public string? Email { get; set; }
-    public int? Age { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public int Age { get; set; }
 }

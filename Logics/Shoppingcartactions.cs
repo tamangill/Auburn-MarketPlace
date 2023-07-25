f8f4e34b81574a1f20eaba1fb0910bd217c65c26
Taman Gill <tgill0424@gmail.com>
2023-07-25 12:55:40 -0700

    finished shoppingcart actions

--- Logics/Shoppingcartactions.cs
+++ Logics/Shoppingcartactions.cs
@@ -1,6 +1,6 @@
 using AuburnMarketPlace.Data;
 
-public class Shoppingcartactions : IDisposable{
+public class Shoppingcartactions {
 
   // 1. connect to the product database 
   private readonly MyDbContext _dbContext;
@@ -12,46 +12,54 @@ public class Shoppingcartactions : IDisposable{
   
       public string ShoppingCartId { get; set; }
 
-      public void addToCart(int productID){
+  public bool addToCart( int productID, Cart cart){
+    var item = _dbContext.Product.SingleOrDefault(p => p.productId == productID);
+    if(item == default){
+      return false;
+    }
 
+    var curentCart =  cart.cartItems.SingleOrDefault(cc => cc.productId == productID);
+
+    if(curentCart != default){
+      curentCart.quantity++;
+    } else {
+      var newItem = new CartItem {
+        id = item.productId,
+        quantity = 1,
+        price = item.price,
+        Product = item
+
+      };
+      
+      cart.cartItems.Add(newItem);
+
+
+      try {
+        _dbContext.SaveChanges();
+        return true;
+
+      } catch (Exception ex) { 
+        return false;
       }
+    }
+    return false;
+  }
+  
 
+  public bool removeFromCart(int productID, Cart cart){
+    var cartItem = cart.cartItems.SingleOrDefault(item => item.productId == productID);
 
-  public void Dispose()
-  {
-    throw new NotImplementedException();
+    if( cartItem != default){
+      cart.cartItems.Remove(cartItem);
+      try{
+      _dbContext.SaveChanges();
+      return true;
+      } catch (Exception ex) {
+        return false;
+      }
+    }
+    return false;
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // 2.  Add to cart
-  //        - given the product ID get the cuurent cart 
-  //        - if a cart does not exist then make a cart  
-  //        - make a new cart item and store in the cart
-  //        
-  // 3.  Remove from cart
-  // - remove the cart item 
-  // -  if cart does not exist return a message cart does not exist
-  // - 
-  // -  
 
 }
